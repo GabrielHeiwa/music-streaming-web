4e0ae2efda2cf15f95f0af15cd3e3ee147398922
gabrielheiwa <heiwa.gabriel@gmail.com>
2021-01-15 08:30:23 -0300

    initial create home page

---
+++ src/pages/Home_page/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {
+    HomeContainer
+} from "./style";
+
+const Home: React.FC = () => {
+    return <HomeContainer>
+        Hello worlld
+    </HomeContainer>
+}
+
+export default Home;
---
+++ src/pages/Home_page/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const HomeContainer = styled.body`
+    width: 100vw;
+    height: 100vh;
+`;
+
+export {
+    HomeContainer,
+}
